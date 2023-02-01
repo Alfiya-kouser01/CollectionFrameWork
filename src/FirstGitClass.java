303e1bf7293662f3312779a535300fbed2c6c54d
Alfiya Kouser <alfiyakouser01@gmail.com>
2023-02-01 23:05:42 +0530

    changes done in master first

--- src/FirstGitClass.java
+++ src/FirstGitClass.java
@@ -2,7 +2,7 @@ public class FirstGitClass {
     public static void main(String[] args) {
         System.out.println("This is my first Git commit");
 
-
+        System.out.println("this is done in master");
         System.out.println("Now I'm inside Feature Branch");
 
         System.out.println("2nd Commit in feature Branch");
