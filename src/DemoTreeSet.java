d277597682f10b12d0f9f12c5955bf4a2f6f7501
Alfiya Kouser <alfiyakouser01@gmail.com>
2023-02-07 17:59:08 +0530

    easy java program to solve sudoku

---
+++ src/DemoTreeSet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DemoTreeSet {
+    public static void main(String[] args) {
+        TreeSet t =new TreeSet<>();
+        t.add("a");
+        t.add("A");
+        t.add("B");
+        t.add("Z");
+        t.add("a");
+        t.add("L");
+        System.out.println(t);
+    }
+
+}
